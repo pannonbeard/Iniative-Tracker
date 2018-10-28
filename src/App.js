0b7304b197f31c2ef924aeaa1266f0af24fcf627
Phil Hendricks <pfhendri1@gmail.com>
2018-10-28 18:48:53 -0500

    Set everything up in appjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
+
 import './App.css';
+import AddCharacterForm from './components/AddCharacterForm'
+import CharactersList from './components/CharactersList/CharactersList'
+import InitiativeViewer from './components/InitiativeViewer/InitiativeViewer'
 
 class App extends Component {
+  state = {
+    characters: [
+      { name: 'Steve', initiative: 12 },
+      { name: 'karl', initiative: 9 },
+      { name: 'Alan', initiative: 30 },
+      { name: 'Ann', initiative: 15 }
+    ],
+    initiativeStarted: false,
+    current_character: 0,
+    current_color: ''
+  }
+
+  handleSubmit = (character) => {
+    this.setState( (prevState) => {
+      return {
+        ...prevState,
+        characters: [...prevState.characters, character]
+      }
+    })
+  }
+
+  updateInitiative = (index, initiative) => {
+    this.setState( (prevState) => {
+      let characters = [...prevState.characters]
+      characters[index].initiative = initiative
+      return {
+        ...prevState,
+        characters
+      }
+    })
+  }
+
+  toggleinitiative= () => {
+    this.setState((prevState) => { 
+      let current_color = this.generateColor()
+      let characters = [...prevState.characters].sort(
+         (char_a, char_b) => char_b.initiative - char_a.initiative
+      )
+      return {
+        ...prevState, 
+        characters, 
+        initiativeStarted: !prevState.initiativeStarted,
+        current_color,
+        current_character: 0
+      }
+    })
+  }
+
+  sortByInitiative = () => {
+    this.setState((prevState) => {
+      let characters = [...prevState.characters].sort(
+        (char_a, char_b) => char_b.initiative - char_a.initiative
+      )
+      console.log(characters)
+      return{
+        ...prevState,
+        characters
+      }
+    })
+  }
+
+  goToNextCharacter = () => {
+    this.setState((prevState) => {
+      const current_color = this.generateColor()
+      let current_character = prevState.current_character + 1
+      if(current_character === prevState.characters.length){
+        current_character = 0
+      }
+      return {
+        ...prevState,
+        current_character,
+        current_color
+      }
+    })
+  }
+
+  generateColor(){
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return ( 
+      <div className = "App" >
+        { 
+          this.state.initiativeStarted ?
+            <InitiativeViewer 
+              toggleinitiative={this.toggleinitiative} 
+              character={this.state.characters[this.state.current_character]}
+              nextCharacter={this.goToNextCharacter}
+              background_color={this.state.current_color}
+             />
+            : <React.Fragment>
+                <AddCharacterForm handleSubmit={this.handleSubmit} />
+                <CharactersList characters={this.state.characters} changeInitiative={this.updateInitiative}/>
+                <button 
+                  type='button' 
+                  onClick={this.toggleinitiative}>
+                    Start initiative
+                </button>
+                <button 
+                  type='button' 
+                  onClick={this.sortByInitiative}>
+                    Sort By initiative
+                </button>
+              </React.Fragment>
+        }
+        
+        
       </div>
     );
   }
